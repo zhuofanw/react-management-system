2ed659881a506793e7982989682574366192e5f6
zhuofanw <zhuofanw@sfu.ca>
2021-01-27 19:49:20 +0800

    实现Admin组件布局

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import { Layout } from "antd";
 import memoryUtils from "../../utils/memoryUtils";
+import LeftNav from "../../components/LeftNav";
+import Header from "../../components/Header";
+
+const { Footer, Sider, Content } = Layout;
 export default class Admin extends Component {
 	render() {
 		const { user } = memoryUtils;
@@ -9,6 +14,19 @@ export default class Admin extends Component {
 			//自动跳转到登录界面(在render中)
 			return <Redirect to="/login" />;
 		}
-		return <div>Hello {user.username}!!</div>;
+		return (
+			<Layout style={{ height: "100%" }}>
+				<Sider>
+					<LeftNav />
+				</Sider>
+				<Layout>
+					<Header>Header</Header>
+					<Content style={{ backgroundColor: "#fff" }}>Content</Content>
+					<Footer style={{ textAlign: "center", color: "#cccccc" }}>
+						推荐使用谷歌浏览器，可以获得更佳页面操作体验
+					</Footer>
+				</Layout>
+			</Layout>
+		);
 	}
 }
