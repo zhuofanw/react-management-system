c549876a0a0e69ff931c3431fbab6344df5aea6c
zhuofanw <zhuofanw@sfu.ca>
2021-01-28 14:10:38 +0800

    动态递归显示菜单列表

--- src/components/LeftNav/index.jsx
+++ src/components/LeftNav/index.jsx
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./index.less";
 import logo from "../../assets/images/logo.svg";
-
 import { Menu } from "antd";
 import {
-	AppstoreOutlined,
-	MenuUnfoldOutlined,
-	MenuFoldOutlined,
+	HomeOutlined,
+	UserOutlined,
 	PieChartOutlined,
 	DesktopOutlined,
-	ContainerOutlined,
 	MailOutlined,
 } from "@ant-design/icons";
+import menuList from "../../config/menuConfig";
+import "./index.less";
 
 const { SubMenu } = Menu;
-
 export default class LeftNav extends Component {
+	// 根据menu的数据数组生成对应的标签数组
+	getMenuNodes = (menuList) => {
+		return menuList.map((item) => {
+			if (!item.children) {
+				return (
+					<Menu.Item key={item.key} icon={item.icon}>
+						<Link to={item.key}>{item.title}</Link>
+					</Menu.Item>
+				);
+			} else {
+				return (
+					<SubMenu key={item.key} icon={item.icon} title={item.title}>
+						{this.getMenuNodes(item.children)}
+					</SubMenu>
+				);
+			}
+		});
+	};
 	render() {
 		return (
 			<div className="left-nav">
@@ -30,34 +45,36 @@ export default class LeftNav extends Component {
 					mode="inline"
 					theme="dark"
 				>
-					<Menu.Item key="homePage" icon={<PieChartOutlined />}>
-						首页
+					{/* <Menu.Item key="homePage" icon={<HomeOutlined />}>
+						<Link to="/home">首页</Link>
 					</Menu.Item>
 					<SubMenu key="sub1" icon={<MailOutlined />} title="商品">
 						<Menu.Item key="typeManagement" icon={<MailOutlined />}>
-							品类管理
+							<Link to="/category">品类管理</Link>
 						</Menu.Item>
 						<Menu.Item key="productManagement" icon={<MailOutlined />}>
-							商品管理
+							<Link to="/product">商品管理</Link>
 						</Menu.Item>
 					</SubMenu>
-					<Menu.Item key="userManagement" icon={<DesktopOutlined />}>
-						用户管理
+					<Menu.Item key="userManagement" icon={<UserOutlined />}>
+						<Link to="/user">用户管理</Link>
 					</Menu.Item>
 					<Menu.Item key="roleManagement" icon={<DesktopOutlined />}>
-						角色管理
+						<Link to="/role">角色管理</Link>
 					</Menu.Item>
-					<SubMenu key="sub1" icon={<MailOutlined />} title="图形图表">
+					<SubMenu key="sub2" icon={<MailOutlined />} title="图形图表">
 						<Menu.Item key="barChart" icon={<MailOutlined />}>
-							柱形图
+							<Link to="/charts/bar">柱状图</Link>
 						</Menu.Item>
 						<Menu.Item key="lineChart" icon={<MailOutlined />}>
-							折线图
+							<Link to="/charts/line">折线图</Link>
 						</Menu.Item>
-						<Menu.Item key="pieChart" icon={<MailOutlined />}>
-							饼图
+						<Menu.Item key="pieChart" icon={<PieChartOutlined />}>
+							<Link to="/charts/pie">饼状图</Link>
 						</Menu.Item>
-					</SubMenu>
+					</SubMenu> */}
+
+					{this.getMenuNodes(menuList)}
 				</Menu>
 			</div>
 		);
